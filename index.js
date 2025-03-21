917aae9032c139856415635fd6f2761554b1007a
felipe-lima-flooxai <felipe.lima.flooxai@gmail.com>
2025-03-21 12:56:38 -0300

    refactor: routes are in separated files

--- index.js
+++ index.js
@@ -1,26 +1,11 @@
 const express = require("express");
+let routesIndex = require("./routes/index.js");
+let routesUsers = require("./routes/users.js");
 
 let app = express();
 
-app.get("/",(req, res)=> {
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "text/html");
-    res.end("<h1>Seja bem Vindo</h1>");
-});
-
-app.get("/users",(req, res)=> {
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "application/json");
-    res.json({
-        users: {
-        name:"felipe",
-        contato: "felipe@gmail.com",
-        id: 1
-        }
-    });
-});
-
-
+app.use(routesIndex);
+app.use("/users",routesUsers);
 
 
 
