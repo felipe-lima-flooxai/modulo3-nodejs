c22750e11106f4249b5a8edbd9654563af186cd1
felipe-lima-flooxai <felipe.lima.flooxai@gmail.com>
2025-03-21 11:50:51 -0300

    feat: running a really basic server
    with nodejs

---
+++ index.js
@@ -0,0 +1,9 @@
+const http = require("http");
+
+let server = http.createServer((req, res)=> {
+    console.log("URL: ", req.url);
+    console.log("Method: ", req.method);
+    res.end("OK");
+});
+
+server.listen(3000, '127.0.0.1', ()=> {console.log("Servidor rodando!")});
